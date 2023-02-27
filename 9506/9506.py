f6dad29e47afb4ba03669372bd630405f6ea3f63
JS <zks145@naver.com>
2023-02-28 03:09:53 +0900

    230228

---
+++ 9506/9506.py
@@ -0,0 +1,23 @@
+while True:
+      
+  n = int(input())
+
+  if n == -1:
+    break
+    
+  list = []
+  for i in range(1, n):
+    if n % i == 0:
+      list.append(i)
+
+  s_num = sum(list)
+  print(list)
+  print(s_num)
+
+  if s_num == n:
+     print(f"{n} = ", end="")
+     print(" + ".join(map(str, list)))
+  else:
+    print("{} is NOT perfect.".format(n))
+    
+# print 처음걸 어떻게 해야될지 모르겠다 솔직히 
