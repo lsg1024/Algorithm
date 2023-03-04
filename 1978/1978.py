b056fd0356e1fcb3917dfcac3c4e54fe3a97762c
JS <zks145@naver.com>
2023-03-04 15:57:25 +0900

    230304

--- 1978/1978.py
+++ 1978/1978.py
@@ -1,19 +1,19 @@
 n = int(input())
 
 nums = list(map(int, input().split()))
-
 count = 0
 
-for i in range(n):
-  
-  if nums[i] <= 0:
+for num in nums:
+  result = True
+  if num < 2:
+    result = False
     pass
   else:
-    if nums[i] / 2 or nums[i] / 3 == 0:
-      count += 1
-    else:
-      pass
-
-print(count)
+    for i in range(2, int(num**0.5) + 1):
+      if num % i == 0:
+        result = False
+        break
+  if result == True:
+    count += 1 
     
-  
+print(count)
