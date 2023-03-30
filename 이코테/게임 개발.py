c51f8ceae69654bcfb43e2c2d9e68e63631ef694
JS <zks145@naver.com>
2023-03-31 01:07:43 +0900

    230331 - 실패

--- 이코테/게임 개발.py
+++ 이코테/게임 개발.py
@@ -1,20 +1,34 @@
+# 도저히 못하겠다 아직 내가 풀긴 어려움 ....
+# 게임 크기
 n, m = map(int, input().split())
+
+# 현재 시작 위치
 now_x, now_y, dt = map(int, input().split())
+# 방문지역 필드 만들기
+d = [[0] * m for _ in range(n)]
+
+# 방문 위치 1 표시
+d[now_x][now_y] = 1
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+print(d)
 
 tile = []
 
 for i in range(n):
   tile.append(list(map(int, input().split())))
+  
+tile[now_x][now_y] = 1
 
-# 북 동 남 서 -> 이동 방양 키
-move_way = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+print(tile)
+# while True:
+#   # 반시계 방향으로 탐색 
+#   for move in move_way:
+#     print(move[0], move[1])
 
-while True:
-  # 반시계 방향으로 탐색 
-  for move in move_way:
-    print(move[0], move[1])
-
-  break
+#   break
   
 # print(n, m)
 # print(now_x, now_y, dt)
