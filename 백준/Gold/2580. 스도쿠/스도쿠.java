d4b9ce606d115ac9a7b7da95a6c4f92412bacd4b
JS <92419747+lsg1024@users.noreply.github.com>
2024-05-31 21:20:42 +0900

    [Gold IV] Title: 스도쿠, Time: 520 ms, Memory: 29320 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -3,7 +3,8 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    static int[][] stoke = new int[9][9];
+    static int[][] sudoku = new int[9][9];
+    static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,7 +12,7 @@ class Main {
         for (int i = 0; i < 9; i++) {
             StringTokenizer st= new StringTokenizer(br.readLine());
             for (int j = 0; j < 9; j++) {
-                stoke[i][j] = Integer.parseInt(st.nextToken());
+                sudoku[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
@@ -26,16 +27,16 @@ class Main {
         if (x > 8) {
             for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
-                    System.out.print(stoke[i][j] + " ");
+                    sb.append(sudoku[i][j]).append(" ");
                 }
-                System.out.println();
+                sb.append("\n");
             }
-
+            System.out.println(sb);
             System.exit(0);
         }
 
         // 이미 채워진 부분이면 넘기기
-        if (stoke[x][y] != 0) {
+        if (sudoku[x][y] != 0) {
             if (y == 8) {
                 dfs(x + 1, 0);
             }
@@ -47,45 +48,44 @@ class Main {
         
         // 행, 열, 3 * 3 값이 일치하는지
         for (int n = 1; n <= 9; n++) {
-            boolean isValid = true;
 
-            // 행
-            for (int j = 0; j < 9; j++) {
-                if (stoke[x][j] == n) {
-                    isValid = false;
+            if (isValid(x, y, n)) {
+                sudoku[x][y] = n;
+                if (y == 8) {
+                    dfs(x + 1, 0);
                 }
-            }
-
-            // 열
-            for (int i = 0; i < 9; i++) {
-                if (stoke[i][y] == n) {
-                    isValid = false;
+                else {
+                    dfs(x, y + 1);
                 }
-            }
-
-            // 3 * 3
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (stoke[(x / 3) * 3 + i][(y / 3) * 3 + j] == n) {
-                        isValid = false;
-                    }
-                }
-            }
-
-            if (!isValid) {
-                continue;
-            }
-
-            stoke[x][y] = n;
-
-            if (y == 8) {
-                dfs(x + 1, 0);
-            }
-            else {
-                dfs(x, y + 1);
+                sudoku[x][y] = 0;
             }
             
-            stoke[x][y] = 0;
         }
+    }
+
+    static boolean isValid(int x, int y, int n) {
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[x][i] == n) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[i][y] == n) {
+                return false;
+            }
+        }
+
+        int startX = (x / 3) * 3;
+        int startY = (y / 3) * 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (sudoku[startX + i][startY + j] == n) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
