9d7b812e0a5cc764d7384939c440c2e26d7262ce
JS <92419747+lsg1024@users.noreply.github.com>
2024-06-24 22:47:02 +0900

    [Silver II] Title: N과 M (10), Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+
+    static int n, m;
+    static int[] numbers;
+    static int[] value;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        numbers = new int[n];
+        visit = new boolean[n];
+        
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        value = new int[m];
+        
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int num, int dp) {
+
+        if (dp == m) {
+            for (int val : value) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int bf_num = 0;
+        
+        for (int i = num; i < numbers.length; i++) {
+
+            if (visit[i]) {
+                continue;
+            }
+
+            if (bf_num != numbers[i]) {
+
+                visit[i] = true;
+                
+                value[dp] = numbers[i];
+    
+                bf_num = numbers[i];
+                
+                dfs(i + 1, dp + 1);
+
+                visit[i] = false;
+                
+            }
+                 
+        } 
+    }
+}
