44f136ae1c2ed00f74327e466611882fa294f22f
JS <92419747+lsg1024@users.noreply.github.com>
2024-08-16 06:42:00 +0900

    [level 2] Title: 피로도, Time: 5.48 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    
+    static int maxValue = 0;
+    static boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        
+        dfs(k, dungeons, 0);
+            
+        return maxValue;
+    }
+    
+    static void dfs(int k, int[][] dungeons, int count) {
+        
+        maxValue = Math.max(maxValue, count);
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1] , dungeons, count + 1);
+                visited[i] = false;
+            } 
+        }
+       
+    }
+}
