3a767330d42776183ead0dc5913cebd92d411f2c
JS <92419747+lsg1024@users.noreply.github.com>
2024-08-29 13:20:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 97.53 ms, Memory: 96 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> hashmap = new HashMap<>();
+        
+        // 마라톤 명단
+        for (int i = 0; i < participant.length; i++) {
+            hashmap.put(participant[i], hashmap.getOrDefault(participant[i], 0) + 1);
+        }
+        
+        
+        // 마라톤 완주 명단
+        for (int i = 0; i < completion.length; i++) {
+            hashmap.put(completion[i], hashmap.get(completion[i]) - 1);
+        }
+        
+        for (int i = 0; i < hashmap.size(); i++) {
+            if (hashmap.get(participant[i]) > 0) {
+                answer = participant[i];
+            }
+        }
+        
+        return answer;
+    }
+}
