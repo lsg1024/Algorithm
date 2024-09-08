13cf5df80a47788a2666deb79a0772dd88db0b96
JS <92419747+lsg1024@users.noreply.github.com>
2024-09-08 20:05:45 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14696 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,54 +1,66 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+class Main {
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        sc.nextLine();
+    static int N, M;
+    static int[] dx, dy;
+    static int[][] graph, distance;
+    static boolean[][] visited;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[][] maze = new int[N][M];
-        boolean[][] visited = new boolean[N][M];
-        int[][] distance = new int[N][M];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new int[N][M];
+        distance = new int[N][M];
+        visited = new boolean[N][M];
+
+        dx = new int[] {1, -1, 0, 0};
+        dy = new int[] {0, 0, 1, -1};
 
         for (int i = 0; i < N; i++) {
-            String line = sc.nextLine();
+            String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                maze[i][j] = line.charAt(j) - '0';
+                graph[i][j] = input.charAt(j) - '0';
             }
         }
 
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+        bfs(0, 0);
+        
+    }
 
+    static void bfs(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0});
-        visited[0][0] = true;
-        distance[0][0] = 1;
-
+        queue.offer(new int[] {x, y});
+        visited[x][y] = true;
+        distance[x][y] = 1;
+        
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
+            int xx = current[0];
+            int yy = current[1];
 
-            if (x == N - 1 && y == M - 1) {
-                System.out.println(distance[x][y]);
+            if (xx == N - 1 && yy == M - 1) {
+                System.out.println(distance[N - 1][M - 1]);
                 break;
             }
 
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = xx + dx[i];
+                int ny = yy + dy[i];
 
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && maze[nx][ny] == 1) {
-                    queue.offer(new int[]{nx, ny});
+                if (0 <= nx && nx < N && 0 <= ny && ny < M && !visited[nx][ny] && graph[nx][ny] == 1) {
+                    queue.offer(new int[] {nx, ny});
                     visited[nx][ny] = true;
-                    distance[nx][ny] = distance[x][y] + 1;
+                    distance[nx][ny] = distance[xx][yy] + 1;
                 }
             }
         }
     }
+    
 }
