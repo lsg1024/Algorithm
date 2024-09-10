852559b3994446f1f69c1ca61099eae1895f278e
JS <92419747+lsg1024@users.noreply.github.com>
2024-09-11 01:13:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.11 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 class Solution {
     
-    static int N, M, count;
+    static int N, M;
     static int[] dx, dy;
-    static int[][] visited;
     
     public int solution(int[][] maps) {
         int answer = 0;
@@ -15,38 +14,37 @@ class Solution {
         dx = new int[] {1, -1, 0, 0};
         dy = new int[] {0, 0, 1, -1};
         
-        visited = new int[N][M];
-        
-        count = 0;
-        bfs(maps, 0, 0);
-        
-        return visited[N - 1][M - 1] == 0 ? -1 : visited[N - 1][M - 1];
+        return bfs(maps, 0, 0);
     }
     
-    static void bfs(int[][] maps, int x, int y) {
+    static int bfs(int[][] maps, int x, int y) {
         
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {x, y});
-        visited[x][y] = 1;
+        queue.offer(new int[] {x, y, 1});
 
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
             int nx = current[0];
             int ny = current[1];
+            int distance = current[2];
+            
+            if (nx == N - 1 && ny == M - 1) {
+                return distance;
+            }
             
             for (int i = 0; i < 4; i++) {
                 int nnx = nx + dx[i];
                 int nny = ny + dy[i];
                 
-                
-                
-                if (0 <= nnx && nnx < N && 0 <= nny && nny < M && visited[nnx][nny] == 0 && maps[nnx][nny] == 1) {
-                    visited[nnx][nny] = visited[nx][ny] + 1;
-                    queue.offer(new int[] {nnx, nny});
+                if (0 <= nnx && nnx < N && 0 <= nny && nny < M && maps[nnx][nny] == 1) {
+                    maps[nnx][nny] = 0;
+                    queue.offer(new int[] {nnx, nny, distance + 1});
                 }  
               
             }
         }
+        
+        return -1;
                
     }
 }
