eb8a84c7cc7512efef45e930673e321a18c48410
JS <92419747+lsg1024@users.noreply.github.com>
2024-09-11 23:56:25 +0900

    [Silver I] Title: 영역 구하기, Time: 112 ms, Memory: 15872 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// 왼쪽 아래, 오른쪽 위
+
+class Main {
+
+    static int M, N, K, result, count;
+    static int[][] graph;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken()); // y
+        N = Integer.parseInt(st.nextToken()); // x
+        K = Integer.parseInt(st.nextToken());
+
+        graph = new int[M][N];
+        
+        
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x_1 = Integer.parseInt(st.nextToken());
+            int y_1 = Integer.parseInt(st.nextToken());
+            int x_2 = Integer.parseInt(st.nextToken());
+            int y_2 = Integer.parseInt(st.nextToken());
+
+            for (int y = M - y_2; y < M - y_1; y++) {
+                for (int x = x_1; x < x_2; x++) {
+                    graph[y][x] = 1;
+                }
+            }
+        }
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        result = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (graph[i][j] == 0) {
+                    result++;
+                    count = 0;
+                    arr.add(dfs(i, j));
+                }
+            }
+        }
+
+        Collections.sort(arr);
+
+        System.out.println(result);
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        
+    }
+
+    static int dfs(int y, int x) {
+
+        if (0 <= x && x < N && 0 <= y && y < M) {
+            if (graph[y][x] == 0) {
+                graph[y][x] = 1;
+                count++;
+                dfs(y + 1, x);
+                dfs(y - 1, x);
+                dfs(y, x + 1);
+                dfs(y, x - 1);
+                return count;
+            }
+        }
+        return 0;
+    }
+}
