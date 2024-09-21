18fc8c2f3f40b59189b0f98600aea79bf1e54a03
JS <92419747+lsg1024@users.noreply.github.com>
2024-09-22 00:49:46 +0900

    [level 2] Title: 양궁대회, Time: 7996.91 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    static int[] lion_info, answer;
+    static int MAX_SCORE = Integer.MIN_VALUE;
+    
+    public int[] solution(int n, int[] info) {
+        answer = new int[] { -1 };
+        
+        lion_info = new int[11];
+        
+        dfs(info, 1, n);
+        
+        return answer;
+    }
+    
+    static void dfs(int[] info, int arrows, int n) {
+        
+        if (arrows == n + 1) {
+            int apeach_score = 0;
+            int lion_score = 0;     
+            for (int i = 0; i < 11; i++) {
+                
+                if (info[i] == 0 && lion_info[i] == 0) continue;
+                
+                if (info[i] < lion_info[i]) lion_score += 10 - i;
+                
+                else apeach_score += 10 - i;
+            }
+            
+            // lion_score가 apeach_score보다 크고 MAX_VALUE 보다 크면 갱신
+            if (lion_score > apeach_score) {
+                if (lion_score - apeach_score >= MAX_SCORE) {
+                    MAX_SCORE = lion_score - apeach_score;
+                    answer = lion_info.clone();
+                }
+            }
+            return;
+        }
+        
+        // 화살을 쏴 점수 계산
+        for (int i = 0; i <= 10 && lion_info[i] <= info[i]; i++) {
+            lion_info[i]++;
+            dfs(info, arrows + 1, n);
+            lion_info[i]--;
+        }
+        
+    }
+    
+}
