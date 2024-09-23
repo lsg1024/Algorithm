1fb1e5e289e558844fb6a07eca9d291165309526
JS <92419747+lsg1024@users.noreply.github.com>
2024-09-23 17:44:03 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1592 ms, Memory: 375804 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -6,7 +6,7 @@ class Main {
 
     static int N, M, K;
     static int[] dx, dy;
-    static int[][] map;
+    static int[][] graph;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,13 +18,13 @@ class Main {
 
         dx = new int[] {1, -1, 0, 0};
         dy = new int[] {0, 0, 1, -1};
-        
-        map = new int[N][M];
+
+        graph = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = input.charAt(j) - '0';
+                graph[i][j] = input.charAt(j) - '0';
             }
         }
 
@@ -34,38 +34,44 @@ class Main {
 
     static int bfs() {
         Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {0, 0, 0, 0});
+
         boolean[][][] visited = new boolean[N][M][K + 1];
-        queue.add(new int[] {0, 0, 1, 0});
         visited[0][0][0] = true;
 
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int nx = current[0];
-            int ny = current[1];
-            int ncnt = current[2];
-            int nbreakWall = current[3];
 
-            if (nx == N - 1 && ny == M - 1) {
-                return ncnt;
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            int cnt = current[2];
+            int isWall = current[3];
+
+            if (x == N - 1 && y == M - 1) {
+                return cnt + 1;
             }
 
             for (int i = 0; i < 4; i++) {
-                int nnx = nx + dx[i];
-                int nny = ny + dy[i];
-                if (0 <= nnx && nnx < N && 0 <= nny && nny < M) {
-                    if (map[nnx][nny] == 1 && nbreakWall + 1 <= K && !visited[nnx][nny][nbreakWall + 1]) {
-                        visited[nnx][nny][nbreakWall + 1] = true;
-                        queue.add(new int[] {nnx, nny, ncnt + 1, nbreakWall + 1});
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
+
+                    if (graph[nx][ny] == 0 && !visited[nx][ny][isWall]) {
+                        queue.offer(new int[] {nx, ny, cnt + 1, isWall});
+                        visited[nx][ny][isWall] = true;
                     }
-                    else if (map[nnx][nny] == 0 && !visited[nnx][nny][nbreakWall]) {
-                        visited[nnx][nny][nbreakWall] = true;
-                        queue.add(new int[] {nnx, nny, ncnt + 1, nbreakWall});
-                    }
+
+                    if (graph[nx][ny] == 1 && isWall < K && !visited[nx][ny][isWall + 1]) {
+                        queue.offer(new int[] {nx, ny, cnt + 1, isWall + 1});
+                        visited[nx][ny][isWall + 1] = true;
+                    } 
                 }
-            }
-            
+            }   
         }
+
         return -1;
+        
     }
     
 }
