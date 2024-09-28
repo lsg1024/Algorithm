7f60bc3b98fb4221d20b3fc800648515d2f1c3de
JS <92419747+lsg1024@users.noreply.github.com>
2024-09-28 22:08:57 +0900

    [Silver II] Title: 종이의 개수, Time: 956 ms, Memory: 314060 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -1,70 +1,72 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 class Main {
-    static int n;
-    static int[][] arr;
-    static int[] total;
+
+    static int N;
+    static int[] papers = new int[3];
+    static int[][] graph;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+
+        graph = new int[N][N];
+
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        isPaper(0, 0, N);
+
+        for (int paper : papers){
+            System.out.println(paper);
+        }
         
-        arr = new int[n][n];
-        total = new int[3]; // -1, 0, 1
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        paperNum(0, 0, n);
-
-        for (int t : total) {
-            System.out.println(t);
-        }
     }
 
-    static void paperNum(int x, int y, int size) {
-        if (check(x, y, size)) {
-            int num = arr[x][y];
-            if (num == -1) {
-                total[0]++;
-            }
-            else if (num == 0) {
-                total[1]++;
-            }
-            else {
-                total[2]++;
-            }
-            return; 
+    static void isPaper(int x, int y, int size) {
+        if (isCheck(x, y, size)) {
+            int number = graph[x][y];
+            
+            if (number == -1) papers[0] += 1;
+            else if (number == 0) papers[1] += 1;
+            else papers[2] += 1;
+            return;
         }
 
-        int newSize = size / 3;
+        int papers_size = size / 3;
 
-        paperNum(x, y, newSize);
-        paperNum(x, y + newSize, newSize);
-        paperNum(x, y + 2 * newSize, newSize);
-        paperNum(x + newSize, y, newSize);
-        paperNum(x + 2 * newSize, y, newSize);
-        paperNum(x + newSize, y + newSize, newSize);
-        paperNum(x + 2 * newSize, y + newSize, newSize);
-        paperNum(x + newSize, y + 2 * newSize, newSize);
-        paperNum(x + 2 * newSize, y + 2 * newSize, newSize);
+        // 하나의 큰 종이별로 9개의 조각이 나옴
+        isPaper(x, y, papers_size);
+        isPaper(x, y + papers_size, papers_size);
+        isPaper(x, y + 2 * papers_size, papers_size);
+        isPaper(x + papers_size, y, papers_size);
+        isPaper(x + 2 * papers_size, y, papers_size);
+        isPaper(x + papers_size, y + papers_size, papers_size);
+        isPaper(x + 2 * papers_size, y + papers_size, papers_size);
+        isPaper(x + papers_size, y + 2 * papers_size, papers_size);
+        isPaper(x + 2 * papers_size, y + 2 * papers_size, papers_size);
     }
 
-    static boolean check(int x, int y, int size) {
-        int num = arr[x][y];
+    // 같은 종류 종이 인지
+    static boolean isCheck(int x, int y, int size) {
+        int target = graph[x][y];
         for (int i = x; i < x + size; i++) {
             for (int j = y; j < y + size; j++) {
-                if (arr[i][j] != num) {
+                if (graph[i][j] != target) {
                     return false;
                 }
             }
         }
         return true;
     }
+    
 }
