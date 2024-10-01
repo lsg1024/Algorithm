37643341539cd6fba6cf1f6f0ef7aa781afb31cc
JS <92419747+lsg1024@users.noreply.github.com>
2024-10-01 13:58:26 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 10.64 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -7,44 +7,44 @@ class Solution {
         int numberOfArea = 0;
         int maxSizeOfOneArea = 0;
         
-        boolean[][] visited = new boolean[picture.length][picture[0].length];
+        boolean[][] visited = new boolean[m][n];
         
-        for (int i = 0; i < picture.length; i++) {
-            for (int j = 0; j < picture[i].length; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (!visited[i][j] && picture[i][j] != 0) {
                     numberOfArea++;
-                    int currentAreaSize = dfs(i, j, picture, visited);
-                    if (maxSizeOfOneArea < currentAreaSize) {
-                        maxSizeOfOneArea = currentAreaSize;
+                    int currentSize = dfs(m, n, i, j, picture, visited);
+                    
+                    if (currentSize > maxSizeOfOneArea) {
+                        maxSizeOfOneArea = currentSize;
                     }
                 }
             }
         }
-        
+
         int[] answer = new int[2];
         answer[0] = numberOfArea;
         answer[1] = maxSizeOfOneArea;
-        
         return answer;
     }
     
-    static int dfs(int x, int y, int[][] picture, boolean[][] visited) {
+    static int dfs(int m, int n, int x, int y, int[][] picture, boolean[][] visited) {
         
         int target = picture[x][y];
         int count = 1;
         
-        visited[x][y] = true;        
+        visited[x][y] = true;
         
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             
-            if (0 <= nx && nx < picture.length && 0 <= ny && ny < picture[0].length && !visited[nx][ny] && picture[nx][ny] == target) {
-                count += dfs(nx, ny, picture, visited);
-            }
+            if (0 <= nx &&  nx < m && 0 <= ny && ny < n && !visited[nx][ny] && picture[nx][ny] == target) {
+            count += dfs(m, n, nx, ny, picture, visited);
         }
+        } 
         
         return count;
+        
     }
-    
 }
