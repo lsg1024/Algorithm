1622b16868aa2aee7b1951738c8a479efe7b30b8
JS <92419747+lsg1024@users.noreply.github.com>
2024-10-01 16:12:03 +0900

    [level 2] Title: 124 나라의 숫자, Time: 1.86 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,33 +1,12 @@
-import java.util.*;
-
 class Solution {
-    
-    static int[] numbers = new int[] {1, 2, 4};
-    
-    public String solution(int n) {
-        String answer = "";
-        
-        long total = 0;
-        int k = 0;
-        
-        //몇번째 자리인지 구하기
-        while (total < n) {
-            k++;
-            total += Math.pow(3, k);
-        }
-        
-         //k자리수 내 위치 결정
-        long previousTotal = total - (long)Math.pow(3, k);
-        long position = n - previousTotal - 1;
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < k; i++) {
-            long p = (long)Math.pow(3, k - i - 1);
-            int idx = (int)(position / p);
-            sb.append(numbers[idx]);
-            position %= p;
-        }
-        
-        return sb.toString();
-    }
+  public String solution(int n) {
+      String[] num = {"4","1","2"};
+      String answer = "";
+
+      while(n > 0){
+          answer = num[n % 3] + answer;
+          n = (n - 1) / 3;
+      }
+      return answer;
+  }
 }
