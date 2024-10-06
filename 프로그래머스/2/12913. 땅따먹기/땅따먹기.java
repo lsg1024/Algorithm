5d14b5780d5c04177dffcb13185097828623e663
JS <92419747+lsg1024@users.noreply.github.com>
2024-10-06 16:08:18 +0900

    [level 2] Title: 땅따먹기, Time: 20.36 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    
+    
+    int solution(int[][] land) {
+        int answer = 0;
+        
+        int n = land.length;
+        int m = land[0].length;
+        
+        for (int i = 1; i < n; i++) {
+            
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][2], Math.max(land[i - 1][3], land[i - 1][0]));
+            land[i][2] += Math.max(land[i - 1][3], Math.max(land[i - 1][1], land[i - 1][0]));
+            land[i][3] += Math.max(land[i - 1][2], Math.max(land[i - 1][1], land[i - 1][0]));
+            
+        }
+        
+        for (int i = 0; i < m; i++) {
+            answer = Math.max(land[n - 1][i], answer);
+        }
+        
+
+        return answer;
+    }
+    
+ 
+}
