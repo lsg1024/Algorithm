504b4fccd3883bd6c05ed036276db51c0ca751ea
JS <92419747+lsg1024@users.noreply.github.com>
2024-10-11 19:47:47 +0900

    [level 2] Title: 단체사진 찍기, Time: 1925.61 ms, Memory: 199 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,114 +1,88 @@
-import java.util.*;
-
 class Solution {
-    
-    static char[] friends = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-    static int count;
-    static List<Condition> conditions;
-    
-    static class Condition {
-        char first;
-        char second;
-        char operator;
-        int distance;
-        
-        Condition(char first, char second, char operator, int distance) {
-            this.first = first;
-            this.second = second;
-            this.operator = operator;
-            this.distance = distance;
-        }
-    }
-    
-    public int solution(int n, String[] data) {
-        count = 0;
-        conditions = new ArrayList<>();
-        
-        for (String condition : data) {
-            char first = condition.charAt(0);
-            char second = condition.charAt(2);
-            char operator = condition.charAt(3);
-            int distance = condition.charAt(4) - '0';
-            conditions.add(new Condition(first, second, operator, distance));
-        }
-        
-        boolean[] used = new boolean[8];
-        
-        backtrack(new ArrayList<>(), used);  
-        
-        return count;
-    }
-    
-    static void backtrack(List<Character> order, boolean[] used) {
-        
-        if (order.size() == 8) {
-            if (isValid(order)) {
-                count++;
-            }
-            return;
-        }
-        
-        // 프렌즈 배치
-        for (int i = 0; i < 8; i++) {
-            if (!used[i]) {
-                order.add(friends[i]);
-                used[i] = true;
-                
-                // 배치 가능 여부
-                if (isPartialValid(order)) {
-                    backtrack(order, used);
-                } 
-                
-                order.remove(order.size() - 1);
-                used[i] = false;
-            }
-        }
-        
-    }
-    
-    static boolean isValid(List<Character> order) {
-        
-        for (Condition condition : conditions) {
-            int p1 = order.indexOf(condition.first);
-            int p2 = order.indexOf(condition.second);
-            int p_distance = Math.abs(p1 - p2) - 1;
-            
-            if (condition.operator == '=') {
-                if (p_distance != condition.distance) return false;
-            } else if (condition.operator == '<') {
-                if (p_distance >= condition.distance) return false;
-            } else if (condition.operator == '>') {
-                if (p_distance <= condition.distance) return false;
-            }      
-        }
-        return true;
-    }
-    
-    static boolean isPartialValid(List<Character> order) {
-        
-        HashMap<Character, Integer> pMap = new HashMap<>();
-        for (int i = 0; i < order.size(); i++) {
-            pMap.put(order.get(i), i);
-        }
-        
-        for (Condition condition : conditions) {
-            
-            if (pMap.containsKey(condition.first) && pMap.containsKey(condition.second)) {
-                int p1 = order.indexOf(condition.first);
-                int p2 = order.indexOf(condition.second);
-                int p_distance = Math.abs(p1 - p2) - 1;
+  public int solution(int n, String[] data) {
+        char[] s = new char[]{'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
 
-                if (condition.operator == '=') {
-                    if (p_distance != condition.distance) return false;
-                } else if (condition.operator == '<') {
-                    if (p_distance >= condition.distance) return false;
-                } else if (condition.operator == '>') {
-                    if (p_distance <= condition.distance) return false;
+        int count = 0;
+
+        for (int a = 0; a < s.length; a++) {
+            for (int b = 0; b < s.length; b++) {
+                if (b == a) {
+                    continue;
                 }
+                for (int c = 0; c < s.length; c++) {
+                    if (c == a || c == b) {
+                        continue;
+                    }
+                    for (int d = 0; d < s.length; d++) {
+                        if (d == c || d == a || d == b) {
+                            continue;
+                        }
+                        for (int e = 0; e < s.length; e++) {
+                            if (e == d || e == a || e == b || e == c) {
+                                continue;
+                            }
+                            for (int f = 0; f < s.length; f++) {
+                                if (f == e || f == d || f == c || f == b || f == a) {
+                                    continue;
+                                }
+                                for (int g = 0; g < s.length; g++) {
+                                    if (g == a || g == b || g == c || g == d || g == e || g == f) {
+                                        continue;
+                                    }
+                                    for (int h = 0; h < s.length; h++) {
+                                        if (h == a || h == b || h == c || h == d || h == e || h == f || h == g) {
+                                            continue;
+                                        }
+                                        String makeS = "" + s[a] + s[b] + s[c] + s[d] + s[e] + s[f] + s[g] + s[h];
+
+                                        boolean isCheck = false;
+                                        for (int i = 0; i < data.length; i++) {
+                                            char first = data[i].charAt(0);
+                                            char second = data[i].charAt(2);
+                                            char inequality = data[i].charAt(3);
+                                            int distance = data[i].charAt(4) - 48;
+
+                                            int firstIndex = makeS.indexOf(first);
+                                            int secondIndex = makeS.indexOf(second);
+                                            switch (inequality) {
+                                                case '=': {
+                                                    if (Math.abs(firstIndex - secondIndex) == distance + 1) {
+                                                        isCheck = true;
+                                                    } else
+                                                        isCheck = false;
+                                                    break;
+                                                }
+                                                case '>': {
+                                                    if (Math.abs(firstIndex - secondIndex) > distance + 1) {
+                                                        isCheck = true;
+                                                    } else
+                                                        isCheck = false;
+                                                    break;
+                                                }
+                                                case '<': {
+                                                    if (Math.abs(firstIndex - secondIndex) < distance + 1) {
+                                                        isCheck = true;
+                                                    } else
+                                                        isCheck = false;
+                                                    break;
+                                                }
+                                            }
+                                            if (isCheck == false) {
+                                                break;
+                                            }
+                                        }
+                                        if (isCheck) {
+                                            count++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
             }
-            
         }
-        return true;
-    }
-    
+        return count;
+  }
 }
