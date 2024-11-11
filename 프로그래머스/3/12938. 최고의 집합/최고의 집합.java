cbe759e6c28efe8b7cf3b99be4e6addf42e5ce89
JS <92419747+lsg1024@users.noreply.github.com>
2024-11-11 23:21:44 +0900

    [level 3] Title: 최고의 집합, Time: 0.98 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        int[] answer;
+        
+        if (s == 1 || n > s) {
+            return new int[] {-1};
+        }
+        
+        int size = n;
+        answer = new int[n];
+        
+        for (int i = 0; i < size; i++) {
+            int value = s / n;
+            answer[i] = value;
+            n--;
+            s -= value;
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
