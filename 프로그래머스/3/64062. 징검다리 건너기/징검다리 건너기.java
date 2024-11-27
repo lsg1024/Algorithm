e7d0eb02e6f53a0403b1884537a25aa5e6ceb059
JS <92419747+lsg1024@users.noreply.github.com>
2024-11-27 13:07:11 +0900

    [level 3] Title: 징검다리 건너기, Time: 57.78 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] stones, int k) {  
+        int low = 0;
+        int high = 0;
+        for (int stone : stones) {
+            high = Math.max(stone, high);
+        }
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            int distance = 0;
+            boolean canCross = true;
+            
+            for (int i = 0; i < stones.length; i++) {
+                if (stones[i] - mid <= 0) {
+                    distance++;
+                    
+                    if (distance >= k) {
+                        canCross = false;
+                        break;
+                    }
+                } else {
+                    distance = 0;
+                }
+            }
+            
+            if (canCross) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+            
+        }
+        
+        return low;
+    }
+}
