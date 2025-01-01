ca59a949d896a3c943cc9942a0101900136cca29
JS <92419747+lsg1024@users.noreply.github.com>
2025-01-01 15:39:46 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,20 @@
+SELECT 
+    HG.SCORE,
+    HG.EMP_NO,
+    HE.EMP_NAME,
+    HE.POSITION,
+    HE.EMAIL
+FROM (
+    SELECT 
+        EMP_NO, 
+        SUM(SCORE) AS SCORE 
+    FROM HR_GRADE 
+    GROUP BY 
+        EMP_NO
+    ORDER BY 
+        SCORE DESC
+    LIMIT 1
+) HG
+JOIN HR_EMPLOYEES HE
+    ON HE.EMP_NO = HG.EMP_NO
+
