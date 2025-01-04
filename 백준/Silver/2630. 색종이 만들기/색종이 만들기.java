4cce453a640bb8b7f2073b61d6c73cac0e550a67
JS <92419747+lsg1024@users.noreply.github.com>
2025-01-04 15:48:10 +0900

    [Silver II] Title: 색종이 만들기, Time: 136 ms, Memory: 15616 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -4,67 +4,50 @@ import java.io.*;
 
 class Main {
 
-    static int N;
-    static int[] result;
+    static int[] colors;
     static int[][] graph;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        
-        result = new int[2];
+        int N = Integer.parseInt(st.nextToken());
+
         graph = new int[N][N];
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine()); 
             for (int j = 0; j < N; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 분할탐색
-        func_1(0, 0, N);
+        colors = new int[2];
+        dfs(N, 0, 0);
 
-        for (int num : result) {
-            System.out.println(num);
-        }
-        
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
     }
 
-    static void func_1(int x, int y, int size) {
-
-        if (func_2(x, y, size)) {
-            int target = graph[x][y];
-            result[target]++;
-            return;
-        } else {
-            // 사분면 위치에서 false가 나온 경우 즉 일치 하지 않은 경우
-            size /= 2;
-            
-            func_1(x, y, size);
-            func_1(x + size, y, size);
-            func_1(x, y + size, size);
-            func_1(x + size, y + size, size);
-        }
-        
-    }
-
-    static boolean func_2(int x, int y, int size) {
+    static void dfs(int n, int x, int y) {
 
         int target = graph[x][y];
-
-        for (int i = x; i < x + size; i++) {
-            for (int j = y; j < y + size; j++) {
+        
+        for (int i = x; i < x + n; i++) {
+            for (int j = y; j < y + n; j++) {
                 if (target != graph[i][j]) {
-                    return false;
+                    int half = n / 2;
+                    dfs(half, x, y);
+                    dfs(half, x, y + half);
+                    dfs(half, x + half, y);
+                    dfs(half, x + half, y + half);
+                    return;
                 }
             }
         }
 
-        return true;
+        colors[target]++;
+        return;
         
     }
-    
 }
