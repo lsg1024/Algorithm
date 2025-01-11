5cfb603a9f1e47323305e7e420a5ff972e704c1c
JS <92419747+lsg1024@users.noreply.github.com>
2025-01-11 18:50:55 +0900

    [Gold IV] Title: 타일 채우기, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n % 2 != 0) {
+            System.out.println(0);
+            return;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[2] = 3;
+
+        for (int i = 4; i <= n; i+=2) {
+            dp[i] = dp[i - 2] * 3;
+            for (int j = i - 4; j >= 0; j-=2) {
+                dp[i] += dp[j] * 2;
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
