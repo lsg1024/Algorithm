5ed16e0a71c78725957f146c5ba17b42adb5c027
JS <92419747+lsg1024@users.noreply.github.com>
2025-01-15 22:23:16 +0900

    [Silver III] Title: N과 M (3), Time: 388 ms, Memory: 68524 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -4,38 +4,34 @@ import java.io.*;
 
 class Main {
 
-    static int n, m;
-    static int[] value;
+    static int N, M;
+    static int[] nums;
     static StringBuilder sb = new StringBuilder();
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        value = new int[m];
         
-        dfs(0);
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[M];
+        func(0);
         System.out.println(sb);
     }
 
-    static void dfs(int dp) {
-
-        if (dp == m) {
-            for (int val : value) {
-                sb.append(val).append(" ");
+    static void func(int depth) {
+        if (depth == M) {
+            for (int num : nums) {
+                sb.append(num).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
-                value[dp] = i;
-                dfs(dp + 1);
-
-        } 
+        for (int i = 1; i < N + 1; i++) {
+            nums[depth] = i;
+            func(depth + 1);
+        }
         
     }
 }
