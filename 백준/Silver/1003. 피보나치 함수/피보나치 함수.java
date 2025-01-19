a095e0049a2dc131b557a67360de1aa5fdecc07b
JS <92419747+lsg1024@users.noreply.github.com>
2025-01-19 19:29:13 +0900

    [Silver III] Title: 피보나치 함수, Time: 128 ms, Memory: 15936 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -3,15 +3,12 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-
-    static int[][] dp;
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
-        dp = new int[41][2];
+        int[][] dp = new int[41][2];
         dp[0][0] = 1;
         dp[0][1] = 0;
         dp[1][0] = 0;
@@ -21,10 +18,19 @@ class Main {
             dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
             dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
         }
-        
-        for (int i = 0; i < T; i++) {
-            int num = Integer.parseInt(br.readLine());
-            System.out.println(dp[num][0] + " " + dp[num][1]);    
+    
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n == 0) {
+                System.out.println(1 + " " + 0);
+                continue;
+            } else if (n == 1) {
+                System.out.println(0 + " " + 1);
+                continue;
+            }
+
+            System.out.println(dp[n][0] + " " + dp[n][1]);
         }
     }
 }
