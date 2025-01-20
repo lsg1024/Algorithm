ccc609e0c96a15aea6a1bb28bbc7f29f3713dca2
JS <92419747+lsg1024@users.noreply.github.com>
2025-01-20 14:44:10 +0900

    [Gold III] Title: ACM Craft, Time: 708 ms, Memory: 239656 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,73 +1,77 @@
- import java.util.*;
+import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 class Main {
 
-    static int target;
-    static int[] times, maxTime, inDegree;
+    static int T, N, K;
+    static int[] dp, costs, inDegree;
     static ArrayList<ArrayList<Integer>> graph;
-    static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int T = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st;
-        for (int i = 0; i < T; i++) {
+        for (int t = 0; t < T; t++) {
             st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
 
-            times = new int[N + 1];
-            maxTime = new int[N + 1];
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            costs = new int[N + 1];
+            graph = new ArrayList<>();
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                times[j] = Integer.parseInt(st.nextToken());
+            for (int i = 1; i < N + 1; i++) {
+                costs[i] = Integer.parseInt(st.nextToken());
             }
 
-            graph = new ArrayList<>();
-            for (int j = 0; j <= N; j++) {
+            for (int i = 0; i < N + 1; i++) {
                 graph.add(new ArrayList<>());
             }
 
             inDegree = new int[N + 1];
-            for (int j = 1; j <= K; j++) {
+            for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
                 int start = Integer.parseInt(st.nextToken());
                 int end = Integer.parseInt(st.nextToken());
+
                 graph.get(start).add(end);
                 inDegree[end]++;
             }
 
-            target = Integer.parseInt(br.readLine());
-            bfs(N);
+            int target = Integer.parseInt(br.readLine());
+
+            bfs(target);
         }
-        System.out.println(sb);
     }
 
-    static void bfs(int N) {
+    static void bfs(int target) {
         Queue<Integer> queue = new LinkedList<>();
 
+        dp = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            if (inDegree[i] == 0) {
+            if (inDegree[i] == 0) { // 진입 차수가 0인 것을 찾음 그것이 start인 부분
                 queue.offer(i);
-                maxTime[i] = times[i];
+                dp[i] = costs[i];
             }
         }
-        
+
         while (!queue.isEmpty()) {
-            int node = queue.poll();
-            
-            for (int next : graph.get(node)) {
-                maxTime[next] = Math.max(maxTime[next], maxTime[node] + times[next]);
-                if (--inDegree[next] == 0) {
-                    queue.offer(next);
+            int index = queue.poll();
+
+            for (int next : graph.get(index)) {
+                inDegree[next]--;
+                dp[next] = Math.max(dp[next], costs[next] + dp[index]);
+
+                if (inDegree[next] == 0) {
+                     queue.offer(next);   
                 }
             }
         }
-        sb.append(maxTime[target]).append("\n");
+        
+        System.out.println(dp[target]);
+        
     }
-    
 }
