cc30b253419fe30c1e607fb08d38f41f67b61eda
JS <92419747+lsg1024@users.noreply.github.com>
2025-01-20 14:46:55 +0900

    [Bronze II] Title: 수 정렬하기, Time: 140 ms, Memory: 14892 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(nums);
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+}
