279f6e5dddcfb416ab011169308d3b6eb48f1587
JS <92419747+lsg1024@users.noreply.github.com>
2025-02-09 00:32:05 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 304 ms, Memory: 27700 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] graph;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                graph[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        int maxSize = 0;
+        int[][] dp = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (graph[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    }
+                    else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+
+                    maxSize = Math.max(dp[i][j], maxSize);
+                }
+            }
+        }
+
+        System.out.println(maxSize * maxSize);
+        
+    }
+
+}
