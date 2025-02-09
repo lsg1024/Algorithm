007a94ca842e7f2202331ad0e85f63d880da4c0c
JS <92419747+lsg1024@users.noreply.github.com>
2025-02-09 15:32:48 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 508 ms, Memory: 46544 KB -BaekjoonHub

---
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        int[] arr;
+        StringTokenizer st;
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            arr = new int[N];
+            
+            st = new StringTokenizer(br.readLine());
+            for (int n = 0; n < N; n++) {
+                arr[n] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+
+            int[] trees = new int[N];
+
+            int mid = N / 2;
+            int left = mid;
+            int right = mid;
+
+            boolean bool = true;
+            while (N-- > 0) {
+
+                if (bool) {
+                    bool = false;
+                    trees[right] = arr[N];
+                    left--;
+                } else {
+                    bool = true;
+                    trees[left] = arr[N];
+                    right++;
+                }
+                
+            }
+
+            int min = Math.abs(trees[trees.length - 1] - trees[0]);
+            for (int i = 0; i < trees.length - 1; i++) {
+                min = Math.max(min, Math.abs(trees[i] - trees[i + 1]));
+            }
+            System.out.println(min);
+        }
+    }
+}
