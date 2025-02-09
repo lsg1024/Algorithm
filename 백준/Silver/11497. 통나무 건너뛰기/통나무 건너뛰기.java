e690be646414a95ae637a8e96aed8c213b089925
JS <92419747+lsg1024@users.noreply.github.com>
2025-02-09 15:34:18 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 468 ms, Memory: 46124 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
@@ -1,54 +1,29 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
 
-        int T = Integer.parseInt(br.readLine());
-
-        int[] arr;
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         for (int t = 0; t < T; t++) {
-            int N = Integer.parseInt(br.readLine());
-            arr = new int[N];
-            
-            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(br.readLine()); // 나무의 개수
+            int[] arr = new int[N];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int n = 0; n < N; n++) {
                 arr[n] = Integer.parseInt(st.nextToken());
             }
 
-            Arrays.sort(arr);
+            Arrays.sort(arr); // 나무 높이 정렬
 
-            int[] trees = new int[N];
-
-            int mid = N / 2;
-            int left = mid;
-            int right = mid;
-
-            boolean bool = true;
-            while (N-- > 0) {
-
-                if (bool) {
-                    bool = false;
-                    trees[right] = arr[N];
-                    left--;
-                } else {
-                    bool = true;
-                    trees[left] = arr[N];
-                    right++;
-                }
-                
+            int maxDiff = 0;
+            for (int i = 2; i < N; i++) {
+                maxDiff = Math.max(maxDiff, Math.abs(arr[i] - arr[i - 2]));
             }
-
-            int min = Math.abs(trees[trees.length - 1] - trees[0]);
-            for (int i = 0; i < trees.length - 1; i++) {
-                min = Math.max(min, Math.abs(trees[i] - trees[i + 1]));
-            }
-            sb.append(min).append("\n");
+            result.append(maxDiff).append("\n");
         }
-        System.out.println(sb);
+        System.out.print(result);
     }
 }
