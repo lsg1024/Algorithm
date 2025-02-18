1bfb7a660d1adf7d5d02673540a0be23c025e4a9
JS <92419747+lsg1024@users.noreply.github.com>
2025-02-18 16:03:43 +0900

    [Silver I] Title: 동물원, Time: 116 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+
+    static int mod = 9901;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[N + 1][3];
+        Arrays.fill(dp[1], 1);
+        
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] += (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % mod;
+            dp[i][1] += (dp[i - 1][0] + dp[i - 1][2]) % mod;
+            dp[i][2] += (dp[i - 1][0] + dp[i - 1][1]) % mod;
+        }
+
+        System.out.println((dp[N][0] + dp[N][1] + dp[N][2]) % mod);
+        
+    }
+    
+}
