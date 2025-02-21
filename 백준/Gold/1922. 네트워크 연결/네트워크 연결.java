dd2de15d2d20704b073ca05ab4350f9b23d46692
JS <92419747+lsg1024@users.noreply.github.com>
2025-02-21 23:19:35 +0900

    [Gold IV] Title: 네트워크 연결, Time: 524 ms, Memory: 49972 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -5,23 +5,21 @@ import java.io.*;
 class Main {
 
     static class Edge implements Comparable<Edge> {
-        int n;
-        int v;
-        int cost;
+        int u, v, weight;
 
-        public Edge(int n, int v, int cost) {
-            this.n = n;
+        public Edge(int u, int v, int weight) {
+            this.u = u;
             this.v = v;
-            this.cost = cost;
+            this.weight = weight;
         }
 
         @Override
         public int compareTo(Edge other) {
-            return this.cost - other.cost;
+            return this.weight - other.weight;
         }
+        
     }
 
-    static int[] parent;
     static int find(int x) {
         if (x == parent[x]) {
             return x;
@@ -39,51 +37,45 @@ class Main {
     }
 
     static int N, M;
-    static List<Edge> edges = new ArrayList<>();
+    static int[] parent;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st;
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
 
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            edges.add(new Edge(x, y, cost));
-        }
-
-        kruskal();
-    }
-
-    static void kruskal() {
-        
         parent = new int[N + 1];
         for (int i = 1; i < N + 1; i++) {
             parent[i] = i;
         }
-        
-        Collections.sort(edges);
 
-        int totalCost = 0;
-        int totalCount = 0;
+        List<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (Edge edge : edges) {
-            if (find(edge.n) != find(edge.v)) {
-                union(edge.n, edge.v);
-                totalCost += edge.cost;
-                totalCount++;
-            }
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-            if (totalCount == N - 1) break;
-            
+            edges.add(new Edge(u, v, w));
         }
 
-        System.out.println(totalCost);
+        Collections.sort(edges);
+
+        int totalCount = 0;
+        int totalWeight = 0;
+        for (Edge edge : edges) {
+            if (find(edge.u) != find(edge.v)) {
+                union(edge.u, edge.v);
+                totalWeight += edge.weight;
+                totalCount++;
+
+                if (totalCount == N - 1) break;
+            }
+        }
+
+        System.out.println(totalWeight);
+        
     }
 }
