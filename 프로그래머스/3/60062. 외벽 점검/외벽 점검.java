b2b868015ebfdcdbf328d9648ce7aef770a8ed53
JS <92419747+lsg1024@users.noreply.github.com>
2025-03-31 13:50:31 +0900

    [level 3] Title: 외벽 점검, Time: 72.95 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Solution {
+    
+    int n, answer;
+    int[] weak, dist, weak_point;
+    
+    public int solution(int n, int[] weak, int[] dist) {
+        answer = Integer.MAX_VALUE;
+        this.n = n;
+        this.weak = weak;
+        this.dist = dist;
+        
+        int len = weak.length;
+        weak_point = new int[len * 2];
+        
+        for (int i = 0; i < len; i++) {
+            weak_point[i] = weak[i];
+            weak_point[i + len] = weak[i] + n;
+        }
+        
+        Arrays.sort(dist);
+        
+        for (int i = 0; i < len; i++) {
+            dfs(i, 0, new boolean[dist.length], new int[dist.length]);
+        }
+        
+        if (answer == Integer.MAX_VALUE) {
+            return -1;
+        }
+        
+        return answer;
+    }
+    
+    public void dfs(int start, int count, boolean[] visited, int[] history) {
+        
+        if (count == dist.length) {
+            answer = Math.min(answer,  check(start, start + weak_point.length / 2, history));
+            return;
+        }
+        
+        for (int i = dist.length - 1; i >= 0; i--) {
+            if (!visited[i]) {
+                visited[i] = true;
+                history[count] = dist[i]; 
+                dfs(start, count + 1, visited, history);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    public int check(int start, int end, int[] history) {
+        int index = 1;
+        int distance = weak_point[start] + history[index - 1];
+        
+        for (int i = start; i < end; i++) {
+            if (distance < weak_point[i]) {
+                index++;
+                if (index > history.length) return Integer.MAX_VALUE;
+                
+                distance = weak_point[i] + history[index - 1];
+            }
+        }
+        return index;
+    }
+    
+    
+}
