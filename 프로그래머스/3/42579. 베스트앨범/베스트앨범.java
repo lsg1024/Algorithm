b63420249433b0d21529c14976b1941236719df5
JS <92419747+lsg1024@users.noreply.github.com>
2026-02-16 23:40:21 +0900

    [level 3] Title: 베스트앨범, Time: 2.50 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,43 +1,58 @@
 import java.util.*;
 
 class Solution {
-    
     public int[] solution(String[] genres, int[] plays) {
         int[] answer;
         
-        HashMap<String, Integer> genresPlayCount = new HashMap<>();
-        HashMap<String, List<int[]>> genresSongs = new HashMap<>();
+        HashMap<String, Integer> types = new HashMap<>();
+        HashMap<String, ArrayList<int[]>> playTimes = new HashMap<>();
         
         for (int i = 0; i < genres.length; i++) {
-            String genre = genres[i];
-            int playtime = plays[i];
+            types.put(genres[i], types.getOrDefault(genres[i], 0) + plays[i]);
             
-            genresPlayCount.put(genre, genresPlayCount.getOrDefault(genre, 0) + playtime);
-            
-            genresSongs.putIfAbsent(genre, new ArrayList<>());
-            genresSongs.get(genre).add(new int[] {i, playtime});    
-        }
-        
-        // 장르별 총 재생 횟수를 기준 정렬 (내림 차순)
-        ArrayList<String> sortedGenres = new ArrayList<>(genresPlayCount.keySet());
-        sortedGenres.sort((g1, g2) -> genresPlayCount.get(g2).compareTo(genresPlayCount.get(g1)));
-        ArrayList<Integer> best = new ArrayList<>();
-        
-        // 장르별 최고 엘범 선정 (
-        // 장르 당 2개 이하인 경우 고려
-        // 장르 당 같은 값인 경우 고려
-        for (String genre : sortedGenres) {
-            List<int[]> songs = genresSongs.get(genre);
-            
-            songs.sort((s1, s2) -> s2[1] == s1[1] ? s1[0] - s2[0] : s2[1] - s1[1]);
-            
-            for (int i = 0; i < Math.min(2, songs.size()); i++) {
-                best.add(songs.get(i)[0]);
+            if (playTimes.get(genres[i]) == null) {
+                playTimes.put(genres[i], new ArrayList<>());
+                playTimes.get(genres[i]).add(new int[] {i, plays[i]});
+            } else {
+                playTimes.get(genres[i]).add(new int[] {i, plays[i]});
             }
-            
         }
         
-        answer = best.stream().mapToInt(i -> i).toArray();
+        for (String key : playTimes.keySet()) {
+            playTimes.get(key).sort((o1, o2) -> {
+                if (o1[1] != o2[1]) {
+                    return o2[1] - o1[1];
+                }
+                return o1[0] - o2[0];
+            });
+        }
+        
+        List<String> genreOrder = new ArrayList<>(types.keySet());
+        genreOrder.sort((o1, o2) -> types.get(o2) - types.get(o1));
+        
+        int index = 0;
+        ArrayList<Integer> tracks = new ArrayList<>();
+        for (String genre : genreOrder) {
+            
+            ArrayList<int[]> play = playTimes.get(genre);
+            
+            int count = 0;
+            for (int i = 0; i < play.size(); i++) {
+                if (count >= 2) {
+                    break;
+                } else {
+                    tracks.add(play.get(i)[0]);
+                    count++;
+                }
+            }
+
+        }
+        
+        answer = new int[tracks.size()];
+        
+        for (int i = 0; i < tracks.size(); i++) {
+            answer[i] = tracks.get(i);
+        }
         
         return answer;
     }
