6254fdb3d208b624e65d84f55e1361b906f83ecb
JS <92419747+lsg1024@users.noreply.github.com>
2026-02-17 15:29:52 +0900

    [level 2] Title: H-Index, Time: 0.96 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,17 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-
+        int answer = 0;
+        
         Arrays.sort(citations);
         
         int n = citations.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = citations[i];
+            citations[i] = citations[n - 1 - i];
+            citations[n - 1 - i] = temp;
+        }
         
+        int h = 0;
         for (int i = 0; i < n; i++) {
-            if (citations[i] >= n - i) {
-                return n - i;
+            if (citations[i] >= i + 1) {
+                h = i + 1;
             }
         }
-
-        return 0;
+        
+        return h;
     }
 }
