a4425726ba91f25f55c8f310f1e1ab9b9ffc01d3
JS <92419747+lsg1024@users.noreply.github.com>
2026-02-19 23:18:39 +0900

    [level 2] Title: 튜플, Time: 46.52 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,47 +1,31 @@
 import java.util.*;
-import java.util.regex.*;
 
 class Solution {
-    public int[] solution(String s) {
-        int[] answer;
+    public ArrayList<Integer> solution(String s) {
+        int[] answer = {};
         
-        String number = "";
-        ArrayList<String> arr = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
+        s = s.substring(2, s.length() - 2);
+        String[] groups = s.split("\\},\\{");
+        
+        Arrays.sort(groups, (o1, o2) -> o1.length() - o2.length());
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        
+        for (String group : groups) {
+            String[] nums = group.split(",");
             
-            if (Character.isDigit(s.charAt(i))) {
-                number = number + Integer.toString(s.charAt(i) - '0');
-            }
-            else {
-                if (number.equals("")) {
-                    continue;
+            for (String num : nums) {
+                int number = Integer.parseInt(num);
+                
+                if (!set.contains(number)) {
+                    result.add(number);
+                    set.add(number);
                 }
-                else {
-                    arr.add(number);
-                    number = "";
-                }
+                
             }
         }
         
-        HashMap<String, Integer> hashmap = new HashMap<>();
-        
-        for (int i = 0; i < arr.size(); i++) {
-            hashmap.put(arr.get(i), hashmap.getOrDefault(arr.get(i), 0) + 1);
-        }
-        
-        ArrayList<Map.Entry<String, Integer>> entryList = new ArrayList<>(hashmap.entrySet());
-        
-        entryList.sort((o1, o2) -> o2.getValue() - o1.getValue());
-        
-        answer = new int[entryList.size()];
-        
-        int index = 0;
-        for (Map.Entry<String, Integer> entry : entryList) {
-            answer[index] = Integer.parseInt(entry.getKey());
-            index++;
-        }
-        
-     
-        return answer;
+        return result;
     }
 }
