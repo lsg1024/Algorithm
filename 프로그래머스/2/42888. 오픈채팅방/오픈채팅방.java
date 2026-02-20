c753aeba830090a26ae07f61b2d506165abcf649
JS <92419747+lsg1024@users.noreply.github.com>
2026-02-20 16:27:41 +0900

    [level 2] Title: 오픈채팅방, Time: 129.06 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,46 +2,42 @@ import java.util.*;
 
 class Solution {
     
-    static HashMap<String, String> nicknames = new HashMap<>();
+    static HashMap<String, String> users = new HashMap<>();
+    static List<String[]> status = new ArrayList<>();
     
     public String[] solution(String[] record) {
         String[] answer;
-        Queue<String[]> IOLogs = new LinkedList<>();
-
+        
         for (int i = 0; i < record.length; i++) {
-            String[] split_record = record[i].split(" ");
+            String[] input = record[i].split(" ");
+
+            String type = input[0];
+            String uuid = input[1];
             
-            String type = split_record[0];
-            String userId = split_record[1];
-            String userName;
-            if (!type.equals("Leave")) {
-                userName = split_record[2];   
-                nicknames.put(userId, userName);
-            }
-            if (!type.equals("Change")) {
-                IOLogs.offer(new String[] {type, userId});    
-            }
-        }
-        
-        answer = new String[IOLogs.size()];
-        
-        int idx = 0;
-        while (!IOLogs.isEmpty()) {
-            String[] current = IOLogs.poll();
-            String tpye = current[0];
-            String userId = current[1];
-            
-            String userName = nicknames.get(userId);
-            if (tpye.equals("Enter")) {
-                answer[idx] = userName + "님이 들어왔습니다.";
+            if (type.equals("Enter")) {
+                String nickname = input[2];
+                users.put(uuid, nickname);
+                
+                status.add(new String[] {uuid, "님이 들어왔습니다."});
+            } else if (type.equals("Leave")) {
+                
+                status.add(new String[] {uuid, "님이 나갔습니다."});
             } else {
-                answer[idx] = userName + "님이 나갔습니다.";
+                String nickname = input[2];
+                users.put(uuid, nickname);
             }
-            idx++;
+            
+            
+        } 
+        
+        answer = new String[status.size()];
+        for (int i = 0; i < status.size(); i++) {
+            String[] stat = status.get(i);
+            String nickname = users.get(stat[0]);
+            
+            answer[i] = nickname + stat[1];
         }
         
         return answer;
     }
-    
 }
-
