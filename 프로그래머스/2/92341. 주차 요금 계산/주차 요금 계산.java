dd60cca8221d15575ab1acd1beef9f68b0dba8f4
JS <92419747+lsg1024@users.noreply.github.com>
2026-02-20 20:34:08 +0900

    [level 2] Title: 주차 요금 계산, Time: 15.32 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,93 +2,70 @@ import java.util.*;
 
 class Solution {
     
-    static int basic_time, basic_fee, charge_time, charge_fee;
-    static HashMap<Integer, Integer> parking = new HashMap<>();
-    static TreeMap<Integer, Integer> parking_bill = new TreeMap<>();
+    static TreeMap<String, Integer> totalTime = new TreeMap<>();
+    static HashMap<String, String> map = new HashMap<>();
     
     public int[] solution(int[] fees, String[] records) {
         int[] answer;
         
-        basic_time = fees[0];
-        basic_fee = fees[1];
-        charge_time = fees[2];
-        charge_fee = fees[3];
+        // 또 출차 후 들어올 수 있음
+        for (String record : records) {
+            String[] s_record = record.split(" ");
+            String time = s_record[0];
+            String car = s_record[1];
+            String type = s_record[2];
+            
+            if (type.equals("IN")) {
+                map.put(car, time);
+                
+            } else {
+                String[] start = map.get(car).split(":");
+                int s_hour = Integer.parseInt(start[0]) * 60;
+                int s_minute = Integer.parseInt(start[1]);
+                int start_time = s_hour + s_minute;
+                
+                String[] end = time.split(":");
+                int e_hour = Integer.parseInt(end[0]) * 60;
+                int e_minute = Integer.parseInt(end[1]);
+                int end_time = e_hour + e_minute;
+                
+                int t_time = end_time - start_time;
+                
+                totalTime.put(car, totalTime.getOrDefault(car, 0) + t_time);
+                
+                map.remove(car);
+            }
+        }
         
-        //누적시간 계산
-        InAndOut(fees, records);
+        for (String key : map.keySet()) {
+            String[] start = map.get(key).split(":");
+            int s_hour = Integer.parseInt(start[0]) * 60;
+            int s_minute = Integer.parseInt(start[1]);
+            int start_time = s_hour + s_minute;
+            
+            int t_time = 1439 - start_time;
+            totalTime.put(key, totalTime.getOrDefault(key, 0) + t_time);
+        }
         
-        //요금 계산
-        answer = new int[parking_bill.size()];
-        int idx = 0;
-        for (Map.Entry<Integer, Integer> bill : parking_bill.entrySet()) {
-            answer[idx] = Pay(bill.getValue());
-            idx++;
+        
+        answer = new int[totalTime.size()];
+        int index = 0;
+        for (String key : totalTime.keySet()) {
+            int time = totalTime.get(key);
+            
+            if (time <= fees[0]) {
+                answer[index++] = fees[1];
+                continue;
+            }
+            
+            int extraTime = time - fees[0];
+            
+            int extraCeliTime = (int) Math.ceil((double) extraTime / fees[2]);
+            
+            answer[index++] = extraCeliTime * fees[3] + fees[1];
         }
         
         return answer;
     }
-    
-    static void InAndOut(int[] fees, String[] records) {
-        
-        for (String record : records) {
-            String[] firstSplit = record.split(":");
-            String time = firstSplit[0];
-            
-            String[] secoundSplit = firstSplit[1].split(" ");
-            String minute = secoundSplit[0];
-            Integer carNumber = Integer.parseInt(secoundSplit[1]);
-            String type = secoundSplit[2];
-            
-            Integer total_time = Integer.parseInt(time) * 60 + Integer.parseInt(minute);
-            
-            if (type.equals("IN")) {
-                parking.put(carNumber, total_time);
-            }
-            else {
-                // 출차 차량 누적 시간
-                Integer start_time = parking.get(carNumber);
-                Total_Time(start_time, total_time, carNumber);
-                parking.remove(carNumber);
-            }
-            
-        }
-        
-        // 24시간 이후에도 존재하는 차량 출차
-        for (Map.Entry<Integer, Integer> entry : parking.entrySet()) {
-            Total_Time(entry.getValue(), 1439, entry.getKey());
-        }
-        
-        return;
-        
-    }
-    
-    // 누적 시간 구하기
-    static void Total_Time(Integer start, Integer end, Integer carNumber) {
-        int using_time = end - start;
-        
-        parking_bill.put(carNumber, parking_bill.getOrDefault(carNumber, 0) + using_time);
-        
-        return;
-        
-    }
-    
-    static int Pay(int using_time) {
-        
-        // 기본 요금 초과 여부
-        if (using_time > basic_time) {
-            
-            // 단위 시간을 초과하지 않는 경우 반올림
-            int upper = 0;
-            if ((using_time - basic_time) % charge_time != 0) {
-                upper = 1;
-            }
-            
-            return basic_fee + ((using_time - basic_time) / charge_time + upper) * charge_fee;
-        }
-        
-        // 기본 요금 이하
-        return basic_fee;
-        
-    }
-    
+
 }
