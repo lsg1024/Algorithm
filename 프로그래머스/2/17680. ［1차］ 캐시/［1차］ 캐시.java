2dc7cc08f669b3b3aa574933dd812406d47f3779
JS <92419747+lsg1024@users.noreply.github.com>
2026-02-20 15:48:35 +0900

    [level 2] Title: [1차] 캐시, Time: 39.85 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -8,31 +8,27 @@ class Solution {
             return cities.length * 5;
         }
         
-        Queue<String> queue = new LinkedList<>();
+        Deque<String> cache = new LinkedList<>();
         
         for (String city : cities) {
             
-            city = city.toUpperCase();
-            
-            if (!queue.isEmpty()) {
-                if (queue.contains(city)) {
-                    queue.remove(city);
-                    queue.offer(city);
-                    answer += 1;
-                } else {
-                    
-                    if (queue.size() < cacheSize) {
-                        queue.offer(city);
-                    } else {
-                        queue.poll();
-                        queue.offer(city);   
-                    }
-                    answer += 5;
-                }
+            city = city.toLowerCase();
+
+            if (cache.contains(city)) {
+
+                cache.remove(city);
+                cache.addLast(city);
+                answer+=1;
+
             } else {
-                queue.offer(city);
-                answer += 5;
+                if (cache.size() >= cacheSize) {
+                    cache.pollFirst();
+                }
+                cache.addLast(city);
+                answer+=5;
             }
+            
+  
         }
         
         return answer;
