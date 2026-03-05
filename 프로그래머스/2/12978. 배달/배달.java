cbe11cf91b984f97dfc8f5e0a49f671c4698ebe8
JS <92419747+lsg1024@users.noreply.github.com>
2026-03-05 14:04:42 +0900

    [level 2] Title: 배달, Time: 3.59 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,76 +2,72 @@ import java.util.*;
 
 class Solution {
     
-    static class Node {
-        int index;
-        int cost;
+    static class Node implements Comparable<Node> {
+        int node;
+        int weight;
         
-        Node (int index, int cost) {
-            this.index = index;
-            this.cost = cost;
+        public Node(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
         }
+        
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.weight, other.weight);
+        }
+        
     }
-    
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-    static int[] visited;
     
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
+
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
         
-        visited = new int[N + 1];
-        Arrays.fill(visited, Integer.MAX_VALUE);
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
         
-        for (int i = 0; i < N + 1; i++) {
-            graph.add(i, new ArrayList<>());
+        for (int i = 0; i < road.length; i++) {
+            int x = road[i][0];
+            int y = road[i][1];
+            int w = road[i][2];
+            
+            graph.get(x).add(new Node(y, w));
+            graph.get(y).add(new Node(x, w));
+        }
+        
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
+        
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(1, 0));
+        
+        while (!pq.isEmpty()) {
+            Node current = pq.poll();
+            int node = current.node;
+            int weight = current.weight;
+            
+            if (weight > dist[node]) continue;
+            
+            for (Node next : graph.get(node)) {
+                int next_node = next.node;
+                int next_weight = next.weight + dist[node];
+                
+                if (next_weight < dist[next_node]) {
+                    dist[next_node] = next_weight;
+                    pq.add(new Node(next_node, next_weight));
+                }
+            }
         }
 
-        for (int[] r : road) {
-            int x = r[0];
-            int y = r[1];
-            int cost = r[2];
-            
-            graph.get(x).add(new Node(y, cost));
-            graph.get(y).add(new Node(x, cost));
-        }
-        
-        dijkstra();
         
         for (int i = 1; i <= N; i++) {
-            if (visited[i] <= K) {
+            if (dist[i] <= K) {
                 answer++;
             }
         }
 
         return answer;
     }
-    
-    static void dijkstra() {
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
-            return o1.cost - o2.cost;
-        });
-        pq.offer(new Node(1, 0));
-        
-        visited[1] = 0;
-        
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            int index = node.index;
-            int cost = node.cost;
-            
-            if (visited[index] < cost) {
-                    continue;
-            }
-            
-            for (Node next : graph.get(index)) {
-                int next_node = next.index;
-                int next_cost = next.cost + cost;
-                
-                if (visited[next_node] > next_cost) {
-                    visited[next_node] = next_cost;
-                    pq.offer(new Node(next_node, next_cost));
-                }
-            }
-        }
-    }
-    
 }
