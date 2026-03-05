1d1efbd4e9dd2cdae93a6633c0b3f78bc7ce6a76
JS <92419747+lsg1024@users.noreply.github.com>
2026-03-05 15:57:26 +0900

    [level 2] Title: 문자열 압축, Time: 4.08 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,40 +1,44 @@
 class Solution {
     public int solution(String s) {
-        int answer = Integer.MAX_VALUE;
+        int answer = s.length();
         
-        if (s.length() == 1) {
-            return 1;
-        }
-        
-        int count = 1;
-        int n = s.length() / 2;
-        for (int i = 1; i <= n; i++) {
-            StringBuilder sb = new StringBuilder();
-            String base = s.substring(0, i);
-            for (int j = i; j <= s.length(); j+=i) {
-                int end = Math.min(i + j, s.length());
-                String target = s.substring(j, end);
+        for (int sc = 1; sc <= s.length() / 2; sc++) {
+            
+            StringBuilder f_word = new StringBuilder();
+            
+            int count = 1;
+            String word = s.substring(0, sc);
+            
+            for (int i = sc; i < s.length(); i+=sc) {
                 
-                if (base.equals(target)) { // 한번이라도 같은 경우에 sb에 추가
+                int end = Math.min(i + sc, s.length());
+                String word2 = s.substring(i, end);
+                
+                if (word.equals(word2)) {
                     count++;
                 } else {
-                    if (count >= 2) { // 2이상이면 압축이 가능한 단계
-                        sb.append(count);
+                    
+                    if (count == 1) {
+                        f_word.append(word);
+                    } else {
+                        f_word.append(count).append(word);
                     }
-                    sb.append(base);
-                    base = target;
+                    
                     count = 1;
+                    word = word2;
+                    
                 }
             }
-            sb.append(base);
-            answer = Math.min(answer, sb.length());
-        }   
+            
+            if (count == 1) {
+                f_word.append(word);
+            } else {
+                f_word.append(count).append(word);
+            }
+            
+            answer = Math.min(answer, f_word.length());
+        }
         
         return answer;
     }
 }
-
-// 2a2ba2c -> 7
-// 2ab2dc2ab2dc -> 2ababcdcd -> 9
-// stack -> 어짜피 1000개 여도 절반 초과되서 같을 수 없음 1~n/2까지 반복
-// StringBuilder로 삽입
