51fd4038e29a8771aeac70e8d271f4022069dc5c
JS <92419747+lsg1024@users.noreply.github.com>
2026-03-05 13:36:32 +0900

    [level 3] Title: 가장 먼 노드, Time: 44.75 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,62 +1,63 @@
 import java.util.*;
 
 class Solution {
-
-    static int maxDistance;
-    static int[] distance;
+    
+    static int maxWeight = 0;
+    static int totalCount = 0;
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static boolean[] visited;
     
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        
-        visited = new boolean[n + 1];
         
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
         
-        for (int[] e : edge) {
-            int x = e[0];
-            int y = e[1];
+        for (int i = 0; i < edge.length; i++) {
+            int x = edge[i][0];
+            int y = edge[i][1];
             
             graph.get(x).add(y);
             graph.get(y).add(x);
         }
         
-        bfs();
-        
-        for (int dis : distance) {
-            if (dis == maxDistance) {
-                answer++;
-            }
-        }
-        
+        answer = bfs(n);
+          
         return answer;
     }
     
-    static void bfs() {
-        Queue<Integer> q = new LinkedList<>();
-        distance= new int[visited.length];
-        Arrays.fill(distance, -1);
+    static int bfs(int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        int[] distances = new int[n + 1];
+        Arrays.fill(distances, -1);
+        distances[1] = 1;
         
-        q.offer(1);
-        visited[1] = true;
-        distance[1] = 1;
-        
-        maxDistance = 1;
-        
-        while (!q.isEmpty()) {
-            int current = q.poll();
-            
-            for (int next : graph.get(current)) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    distance[next] = distance[current] + 1;
-                    maxDistance = Math.max(maxDistance, distance[next]);
-                    q.offer(next);
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+               
+            for (int next : graph.get(node)) {
+                if (distances[next] == -1) {
+                    distances[next] = distances[node] + 1;
+                    queue.add(next);
                 }
             }
         }
+        
+        int maxValue = 0;
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            maxValue = Math.max(distances[i], maxValue);
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            if (distances[i] == maxValue) {
+                count++;
+            }
+        }
+        
+        return count;
+        
     }
+    
 }
