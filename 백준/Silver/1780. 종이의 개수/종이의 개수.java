1bce0ac182df30dfb5e02eade68ec97bec9b96d1
JS <92419747+lsg1024@users.noreply.github.com>
2026-03-06 20:54:09 +0900

    [Silver II] Title: 종이의 개수, Time: 956 ms, Memory: 312852 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -2,68 +2,75 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+// The main method must be in a class named "Main".
 class Main {
 
-    static int N;
-    static int[] papers = new int[3];
-    static int[][] graph;
+    static int N, a, b, c;
+    static int[][] maps;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
 
-        graph = new int[N][N];
-
+        maps = new int[N][N];
         StringTokenizer st;
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
+                maps[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        isPaper(0, 0, N);
+        a = 0;
+        b = 0;
+        c = 0;
+        func(0, 0, N);
 
-        for (int paper : papers){
-            System.out.println(paper);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+
+    static void func(int x, int y, int n) {
+
+        // 같은 종이인지 확인
+        if (isPaper(x, y, n)) {
+            // 종이 나누기
+            int number = maps[x][y];
+            if (number == -1) {
+                a++;
+            } else if (number == 0) {
+                b++;
+            } else {
+                c++;
+            }
+            return;
+            
+        } else {
+            int size = n / 3;
+
+            func(x, y, size);
+            func(x + size, y, size);
+            func(x, y + size, size);
+            func(x + size, y + size, size);
+            func(x + 2*size, y, size);
+            func(x, y + 2*size, size);
+            func(x + 2*size, y + 2*size, size);
+            func(x + size, y + 2 * size, size);
+            func(x + 2 * size, y + size, size);
         }
         
     }
 
-    static void isPaper(int x, int y, int size) {
-        if (isCheck(x, y, size)) {
-            int number = graph[x][y];
-            
-            if (number == -1) papers[0] += 1;
-            else if (number == 0) papers[1] += 1;
-            else papers[2] += 1;
-            return;
-        }
 
-        int papers_size = size / 3;
+    static boolean isPaper(int x, int y, int size) {
 
-        // 하나의 큰 종이별로 9개의 조각이 나옴
-        isPaper(x, y, papers_size);
-        isPaper(x, y + papers_size, papers_size);
-        isPaper(x, y + 2 * papers_size, papers_size);
-        isPaper(x + papers_size, y, papers_size);
-        isPaper(x + 2 * papers_size, y, papers_size);
-        isPaper(x + papers_size, y + papers_size, papers_size);
-        isPaper(x + 2 * papers_size, y + papers_size, papers_size);
-        isPaper(x + papers_size, y + 2 * papers_size, papers_size);
-        isPaper(x + 2 * papers_size, y + 2 * papers_size, papers_size);
-    }
-
-    // 같은 종류 종이 인지
-    static boolean isCheck(int x, int y, int size) {
-        int target = graph[x][y];
+        int number = maps[x][y];
         for (int i = x; i < x + size; i++) {
             for (int j = y; j < y + size; j++) {
-                if (graph[i][j] != target) {
-                    return false;
-                }
+                if (number != maps[i][j]) return false;
             }
         }
         return true;
